e3fc5b4c04f02b85d6e31635477dfcf3c6166f98
Tudor Emanuel <tudorsabau14@gmail.com>
2023-03-28 05:17:39 +0200

    added tf stuff

--- main.tf
+++ main.tf
@@ -7,19 +7,6 @@ resource "google_container_cluster" "primary" {
   location          = "us-central1-c"
   initial_node_count = 4               # number of node (VMs) for the cluster
 
-  # Google recommends custom service accounts that have cloud-platform 
-  # scope and permissions granted via IAM Roles.
-  # for this demo, we'll have no auth set up
-  # master_auth: The aut information for accessing the Kubernetes master.
-  master_auth {
-    username = ""
-    password = ""
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-
   # let's now configure kubectl to talk to the cluster
   provisioner "local-exec" {
     # we will pas the project ID, zone and cluster name here
@@ -50,23 +37,4 @@ resource "google_container_cluster" "primary" {
     create = "45m" 
     update = "60m"
   }
-}
-
-# -------------------------------------------------------------*
-# Next, we create firewall rule to allow access to application
-# note: in our deploy.yml we set and know that
-# The range of valid ports in kubernetes is 30000-32767
-# -------------------------------------------------------------*
-resource "google_compute_firewall" "nodeports" {
-  name    = "vue-port-range"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["30000-32767", "80", "443", "8080", "22", "3000"]  # valid ports in kubernetes is 30000-32767
-  }
-  allow {
-    protocol = "icmp"
-  }
-  source_ranges = ["0.0.0.0/0"]
 }
